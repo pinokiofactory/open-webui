fdf76ff6adbfd69a662be1a36a6ddcbe0340fd44
cocktailpeanut <cocktailpeanuts@proton.me>
2024-09-02 08:28:13 -0400

    update

--- update.js
+++ update.js
@@ -10,5 +10,30 @@ module.exports = {
       path: "app",
       message: "git pull"
     }
+  }, {
+    method: "shell.run",
+    params: {
+      conda: {
+        path: "backend/conda_env",
+      },
+      message: [
+        "{{path.resolve(cwd, local.node_path, 'node') + ' --version'}}",
+        "{{path.resolve(cwd, local.node_path, 'npm') + ' i'}}",
+        "{{path.resolve(cwd, local.node_path, 'npm') + ' run build'}}",
+      ],
+      path: "app"
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      conda: {
+        path: "conda_env",
+      },
+      message: [
+        "pip install -r requirements.txt -U"
+      ],
+      venv: "env",
+      path: "app/backend"
+    }
   }]
 }
