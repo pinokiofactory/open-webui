37a0d764c86cd281d793232d03d452cc37e148bf
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-27 08:02:20 -0500

    update

--- install.js
+++ install.js
@@ -2,17 +2,6 @@ module.exports = {
   "run": [{
     "method": "shell.run",
     "params": {
-      "message": "git clone https://github.com/open-webui/open-webui app"
-    }
-  }, {
-    "method": "fs.copy",
-    "params": {
-      "src": "app/.env.example",
-      "dest": "app/.env"
-    }
-  }, {
-    "method": "shell.run",
-    "params": {
       "venv": "env",
       "venv_python": "3.11",
       "path": "app/backend",
