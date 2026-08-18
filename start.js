d7c59329e3758cac350a1b5f1b834038727bec6c
6Morpheus6 <wanton-decay@gmx.net>
2026-08-18 14:41:04 +0200

    update
    - move data folder out of the environment
    - use new data folder

--- start.js
+++ start.js
@@ -3,9 +3,26 @@ module.exports = async (kernel) => {
   return {
     "daemon": true,
     "run": [{
+      "when": "{{!exists('app/data') && platform === 'win32'}}",
+      "method": "fs.copy",
+      "params": {
+        "src": "app/env/Lib/site-packages/open_webui/data",
+        "dest": "app/data",
+        "options": { "recursive": true }
+      }
+    }, {
+      "when": "{{!exists('app/data') && platform !== 'win32'}}",
+      "method": "fs.copy",
+      "params": {
+        "src": "app/env/lib/python3.11/site-packages/open_webui/data",
+        "dest": "app/data",
+        "options": { "recursive": true }
+      }
+    }, {
       "method": "shell.run",
       "params": {
         "path": "app",
+        "env": { "DATA_DIR": "{{path.resolve(cwd, 'app/data')}}" },
         "venv": "env",
         "message": `open-webui serve --port 42004 --host 127.0.0.1`,
         //"on": [{ "event": "/http://[0-9.:]+/", "done": true }]
